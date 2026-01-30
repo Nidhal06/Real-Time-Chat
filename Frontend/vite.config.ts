cc1dad9eeec1f7876d51b2e1393e9371978b3498
Nidhal Gharbi <97950964+Nidhal06@users.noreply.github.com>
2026-01-30 02:05:15 +0100

    Set base URL for Vite configuration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig(({ mode }) => {
   const apiUrl = process.env.VITE_API_URL ?? 'http://localhost:5000';
 
   return {
+    base: '/',
     plugins: [react()],
     server: {
       port: 5173,
